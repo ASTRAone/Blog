f3f28cdb792ade0615fc5cd679a03302de6af7e5
Vladislav <vladislavmaier819@gmail.com>
2026-01-09 19:17:03 +0300

    fix auth-client

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://blog-m5sj57y77-vladislavs-projects-ac85ce1e.vercel.app",
 });
